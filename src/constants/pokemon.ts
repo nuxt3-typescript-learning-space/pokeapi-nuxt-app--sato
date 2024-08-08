a67b8148f40c7f1ba321901aef718814289d7dac
SATO-14161 <sato.sh.ryota@gmail.com>
2024-08-09 05:37:46 +0900

    :adhesive_bandage: update: aPIエンドポイントの定義を変更

--- src/constants/pokemon.ts
+++ src/constants/pokemon.ts
@@ -1,1 +1,5 @@
-export const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon';
+export const API_BASE_URL = Object.freeze({
+  POKEMON: 'https://pokeapi.co/api/v2/pokemon/',
+  SPECIES: 'https://pokeapi.co/api/v2/pokemon-species/',
+  TYPE: 'https://pokeapi.co/api/v2/type/',
+});
